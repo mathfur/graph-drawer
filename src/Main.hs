0f3790cbc4dd68cc48201b925ac61f9231b06fc7
mathfuru <mathfuru@gmail.com>
2012-08-05 15:17:45 +0900

    Refactor

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,33 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+
 import System.Random
 import Control.Monad.State
 import Helpers
 
-walk :: Int -> State StdGen Int
-walk n = do
-  step <- choice [-1, 1]
-  return (n + step)
+class Optimalize a where
+  walk :: a -> State StdGen a
+  satisfy :: a -> Bool
 
-satisfy :: Int -> Bool
-satisfy n = (n - 50)^2 < 3^2
+instance Optimalize Int where
+  walk n = do
+    step <- choice [-1, 1]
+    return (n + step)
+  satisfy n = (n - 50)^2 < 3^2
 
-calcurateOptimal :: (a -> State StdGen a) -> (a -> Bool) -> Int -> State StdGen a -> State StdGen a
-calcurateOptimal f p max_iterate v0 = do
-  let as = (iterate (f=<<) v0)
-  (return.head.filter p)=<<(sequence as)
+calcurateOptimal :: (Optimalize a) => Int -> State StdGen a -> State StdGen a
+calcurateOptimal max_iterate v0 = do
+  let as = (iterate (walk=<<) v0)
+  (return.head.filter satisfy)=<<(sequence as)
 
-executeWalk :: StdGen -> Int -> Int
-executeWalk g n = fst $ runState (walk n) g
+optimalResult :: (Optimalize a) => a -> State StdGen a
+optimalResult v0 = calcurateOptimal 100 (return v0)
+
+
+evalOptimize :: IO Int
+evalOptimize = do
+  g <- getStdGen
+  return $ evalState (optimalResult (3 :: Int)) g
 
 main = do
-  g <- getStdGen
-  print $ fst $ runState (calcurateOptimal walk satisfy 100 (return 3)) g
+  x <- evalOptimize
+  print x
