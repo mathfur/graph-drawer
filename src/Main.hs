fe7c55d272314b1a101e2f5eb6ebca96eb85f9eb
mathfuru <mathfuru@gmail.com>
2012-08-05 17:36:44 +0900

    中間コミット

--- src/Main.hs
+++ src/Main.hs
@@ -6,18 +6,32 @@ import Helpers
 
 class Optimalize a where
   walk :: a -> State StdGen a
+  cost :: a -> Float
   satisfy :: a -> Bool
 
 instance (Optimalize a) => Optimalize [a] where
   walk as = mapM walk as
-  satisfy as = all satisfy as
+  cost as = foldr (*) 1 $ map cost as
+  satisfy as = cost as < 1
 
 instance Optimalize Int where
   walk n = do
     step <- choice [-1, 1]
     return (n + step)
-  satisfy n = (n - 50)^2 < 3^2
+  cost n = (fromIntegral n - 50)^2
+  satisfy n = cost n < 1
 
+o_walk :: (Optimalize a) => State StdGen a -> State StdGen a
+o_walk v0 = do
+  let v1 = walk=<<v0
+  a0 <- v0
+  a1 <- v1
+  if ((cost a1 <= cost a0) || (prob_p 0.5)) then v1 else v0
+    where
+      prob :: Float -> Float
+      prob t = 1 -  1/1 + t
+      prob_p :: Float -> Bool
+      prob_p t = (0.01 < prob t)
 
 calcurateOptimal :: (Optimalize a) => Int -> State StdGen a -> State StdGen a
 calcurateOptimal max_iterate v0 = do
@@ -25,7 +39,7 @@ calcurateOptimal max_iterate v0 = do
   (return.head.filter satisfy)=<<(sequence as)
 
 optimalResult :: (Optimalize a) => a -> State StdGen a
-optimalResult v0 = calcurateOptimal 10 (return v0)
+optimalResult v0 = calcurateOptimal 1 (return v0)
 
 
 evalOptimize :: IO [Int]
