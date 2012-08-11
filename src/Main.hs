e57cdfc157fd40a7c8a269befee871da81754f8f
mathfuru <mathfuru@gmail.com>
2012-08-11 11:43:28 +0900

    some refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,24 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
 
 import Optimalize
 
-instance Optimalize (Int, Int) where
-  walk (n, m) = do
-    s1 <- step
-    s2 <- step
-    let (n_, m_)  = (n + s1, m + s2)
-    let n__ = if 0 <= n_ then n_ else 0
-    let m__ = if 0 <= m_ then m_ else 0
-    return $ (n__, m__)
-  cost (n, m) = ((fromIntegral n)^2 + (fromIntegral m)^2 - 100)^2
+newtype Point = Point (Int, Int) deriving Show
+
+instance Optimalize Point where
+  walk (Point (n, m)) = do
+    n_ <- walk n
+    m_ <- walk m
+    return $ Point (n_, m_)
+  cost (Point (n, m)) = (cost n) + (cost m) + ((fromIntegral n)^2 + (fromIntegral m)^2 - 100)^2
+
+instance Optimalize Int where
+  walk n = do
+    s <- step
+    return (n+s)
+  cost n = if 0<=n then 0 else ((fromIntegral n)*100)^2
 
 main = do
-  a <- evalOptimize 100 $ (0 :: Int,0 :: Int)
-  let (x, y) = head a
+  a <- evalOptimize 100 $ Point (0,0)
+  let (Point (x, y)) = head a
+  print (x, y)
   print $ x^2 + y^2
